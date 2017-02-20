706fa5a02b50253df5156e971b09c92380c253b6
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2017-02-20 15:28:04 +0900

    test persistent library

--- persistent/testPersistent/app/Main.hs
+++ persistent/testPersistent/app/Main.hs
@@ -1,6 +1,55 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE GADTs #-}
+
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
+
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
 module Main where
 
 import Lib
 
+import Database.Persist
+import Database.Persist.TH
+	(share, mkPersist, sqlSettings, mkMigrate, persistUpperCase)
+import Database.Persist.Sql (runMigration, runSqlConn)
+import Database.Persist.Sqlite (withSqliteConn)
+
+import Control.Monad.Trans.Resource (runResourceT)
+import Control.Monad.Logger (runNoLoggingT)
+import Control.Monad.IO.Class (liftIO)
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistUpperCase|
+Person
+	name		String
+	age		Int Maybe
+BlogPost
+	title		String
+	authorId	PersonId
+|]
+
+database = ":memory:"
+
 main :: IO ()
-main = someFunc
+main = runNoLoggingT $ runResourceT $ withSqliteConn database $ runSqlConn $ do
+	runMigration migrateAll
+
+	johnId <- insert $ Person "John Doe" $ Just 35
+	janeId <- insert $ Person "Jane Doe" $ Nothing
+
+	insert $ BlogPost "My fr1st p0st" johnId
+	insert $ BlogPost "One more for good measure" johnId
+
+	oneJohnPost <- selectList [BlogPostAuthorId ==. johnId] [LimitTo 2]
+	liftIO $ print $ map (blogPostTitle . entityVal) oneJohnPost
+
+	john <- get johnId
+	liftIO $ print $ fmap personName john
+
+	delete janeId
+	deleteWhere [BlogPostAuthorId ==. johnId]
+
+	return ()
